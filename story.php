79436d8ddb2310655b7ee916fcd161d36e92bfb4
Raul Rupsingh <raul@interaxon.ca>
2015-10-16 18:26:34 -0400

    updated to include story link

---
+++ story.php
@@ -0,0 +1,16 @@
+<?php
+//$scheme = $_SERVER['HTTPS'] ? 'https' : 'http';
+//$host = $_SERVER['HTTP_HOST'];
+//$basedir = dirname($_SERVER['SCRIPT_NAME']);
+//header("Location: {$scheme}://{$host}{$basedir}/www.matthewhouse.ca/index.html");
+// exit();
+
+$loader = require __DIR__ . '/vendor/autoload.php';
+$templates = new League\Plates\Engine('templates');
+$current_file_path = dirname(__FILE__);
+$templates->addFolder('about', $current_file_path . '/templates/about');
+
+// Render a template
+echo $templates->render('about::story', ['name' => 'Jonathan']);
+
+?>
---
+++ templates/about/story.php
@@ -0,0 +1,127 @@
+<?php $this->layout('template', ['title' => 'Matthew House | Refugee Reception Services Toronto']) ?>
+
+<!-- overriding styles -->
+<style type="text/css">
+.contenttable, .contenttablesub {
+background-image:url(site/ywd_trialsite_1564/assets/images/white-square-background.jpg);
+background-position:top center;
+background-repeat:repeat;
+}
+.contentmaincol, .contentmaincolsub, .rightcol, .rightcolsub, .leftcol, .breadcrumbs, #contentfooter {
+background-color:transparent;
+}
+#contentfooter {
+background-color:#0079C1;
+}
+body {
+background-image:url(site/ywd_trialsite_1564/assets/images/ice_age__2X.jpg);
+background-position:top left;
+background-repeat:repeat;
+background-attachment:fixed;
+}
+</style>
+
+<table id="outercontenttable" width="100%" border="0" cellspacing="0" cellpadding="0">
+<tr>
+<td>
+<div id="cse" style="width:100%;display:none;"></div>
+<table align="left" class="contenttablesub" border="0" cellspacing="0" cellpadding="0">
+<tr>
+<td class="leftcol">
+<div id="navlefttable">
+<table class="navleft" border="0" cellspacing="0" cellpadding="0">
+<tr>
+<td style="padding-left:0px;">
+</td>
+</tr>
+<tr><td class="navleft2offset"></td></tr>
+<tr><td style="padding-left:0px;"><a href="index3b17.html?pagepath=About_Us/Our_Story&amp;id=44867" class="navleft2current">Our Story</a></td></tr><tr><td style="padding-left:0px;"><a href="indexd05b.html?pagepath=About_Us/Our_Team&amp;id=43700" class="navleft2">Our Team</a></td></tr><tr><td style="padding-left:0px;"><a href="index3993.html?pagepath=About_Us/Our_Houses&amp;id=43698" class="navleft2">Our Houses</a></td></tr><tr><td style="padding-left:0px;"><a href="indexee2f.html?pagepath=About_Us/Media&amp;id=44842" class="navleft2">Media</a></td></tr><tr><td style="padding-left:0px;"><a href="index5a2e.html?pagepath=About_Us/News&amp;id=43703" class="navleft2">News</a></td></tr><tr><td style="padding-left:0px;"><a href="index2c1c.html?pagepath=About_Us/Newsletter&amp;id=68762" class="navleft2">Newsletter</a></td></tr>
+</table>
+</div>
+</td>
+<td class="contentmaincolsub">
+<div id="content">
+<div class="maincolbox" style='clear:both;' controller="1">
+<div class="maincolcblock ywdblock_stacked">
+<div class="wp"><h5>OUR STORY</h5>
+<p></p></div><div style="margin:0px;padding:0px;height:0px;clear:both;"></div>	
+</div>
+</div>
+<div class="maincolbox" style='clear:both;' controller="1">
+<div class="maincolcblock ywdblock_stacked">
+<div class="nwp">
+<iframe id="ywdcblock222032_videoframe" width="560" height="315" src="http://www.youtube.com/embed/tE_Vh4DOXG4?rel=0&amp;" frameborder="0" allowfullscreen></iframe>
+<div style="margin:0px;padding:0px;height:0px;clear:both;"></div>
+<!-- Google Search Result Snippet Ends -->
+</div>	
+</div>
+</div>
+<div class="maincolbox" style='clear:both;' controller="1">
+<div class="maincolcblock ywdblock_stacked">
+<div class="wp"><h2 style="text-align: center;"><span style="color: rgb(128, 128, 128);">&quot;It broke my heart to see refugees were not being welcomed and I longed to do something about it.&quot;</span><br />
+&mdash;<span style="color: rgb(128, 128, 128);">&nbsp;Anne Woolger, Founder &amp; Executive Director</span><img align="bottom" alt="" src="site/ywd_trialsite_1564/assets/images/blue-dotted-line.png" style="width: 678px; height: 30px; line-height: 38px; font-size: 40px;" /></h2>
+<p>&nbsp;</p>
+<h6>THE MATTHEW HOUSE STORY</h6>
+<p><span style="color: rgb(0, 121, 193);">Matthew House grew out of the vision of its founder and current director, Anne Woolger,</span> who had worked for many years at a city-run shelter for refugees in Toronto. There, her eyes were opened to the plight of refugee claimants who were arriving in Canada seeking asylum as refugees&nbsp;but had no place to stay. Recognizing the need for caring shelters geared specifically toward the unique settlement needs of refugee claimants, Anne approached her denominational leaders and shared her vision of establishing a Christian-based reception home that would provide a warm welcome for refugee claimants in a caring home-like setting.</p>
+<p><span style="color: rgb(0, 121, 193);">In 1992, a Board of Directors was formed and Matthew House was incorporated as a non-profit charity. </span>Four years later in October 1996, Anne, appointed as the first director, was mandated to find and establish a permanent home for the refugee ministry. Since opening in 1998, over 1,300 refugees from 88 different nations have been warmly welcomed by caring volunteers and staff who provide the residents help with all their settlement needs, including assistance with paperwork, preparing for the&nbsp;refugee hearing,&nbsp;orientation to Canada, connection with social services and finding and furnishing apartments with donated furniture. Matthew House receives very little direct government support but exists simply through the generosity of individuals, churches, and foundations that care about the plight of refugees.</p>
+<p><span style="color: rgb(0, 121, 193);">Matthew House Toronto has expanded from having just one main shelter to having two additional homes</span> that provide second-stage transitional housing for the most vulnerable refugees who pass through its doors. In all three of its homes, Matthew House continues to fulfill its mission of providing expert settlement support to refugees in beautiful, family-like homes where their dignity and hope are restored.</p>
+<p><span style="color: rgb(0, 121, 193);">Matthew House has also been a catalyst </span>behind the establishment of more than half a dozen other refugee shelters across Canada ( some by the same name) based on the Matthew House Toronto model.</p>
+<p><img alt="" src="site/ywd_trialsite_1564/assets/images/blue-dotted-line.png" style="width: 678px; height: 30px;" />&nbsp;&nbsp;</p>
+<h2><span style="color:0099FF;">Mission Statement</span></h2>
+<p>Matthew House welcomes newly arrived refugees with God&rsquo;s love by providing safe shelter, settlement assistance and bridges into the community&nbsp;while inspiring others through its replicable model and its advocacy.</p>
+<p><img alt="" src="site/ywd_trialsite_1564/assets/images/blue-dotted-line.png" style="width: 678px; height: 30px;" /></p>
+<h2><span style="color:0099FF;">Our Core Values</span></h2>
+<p><span style="color: rgb(0, 121, 193);">Faith Based</span><br />
+Operating out of a Christian faith, we trust in a God who cares and provides for His people. As such, we value prayer and regularly communicate to God our thanks and requests, relying on Him in all circumstances.</p>
+<p><span style="color: rgb(0, 121, 193);">Human Dignity</span><br />
+We believe that each person is created in the image of God, and, therefore sacred. From this conviction we seek to respect, and to protect, the value and dignity of each person staying at Matthew House regardless of their race, religion or political opinion, as well as those we work alongside.</p>
+<p><span style="color: rgb(0, 121, 193);">Holistic Ministry</span><br />
+We recognize that people have physical, spiritual and emotional needs. As such, we value holistic ministry and seek to express that in how we help each person staying with us.</p>
+<p><span style="color: rgb(0, 121, 193);">Community</span><br />
+We value healthy community and work to develop one within Matthew House. We also build bridges with individuals and organizations on behalf of our residents, so that they may continue to experience healthy community beyond their lives at Matthew House.</p>
+<p><span style="color: rgb(0, 121, 193);">Professionalism</span><br />
+We seek to continually develop and upgrade our skills, operations, systems and working environment. We strive to have integrity, professionalism and transparency in all that we do, using our gifts and resources in ways that honour and glorify God.</p>
+<h2>&nbsp;&nbsp;</h2></div><div style="margin:0px;padding:0px;height:0px;clear:both;"></div>	
+</div>
+</div>
+<div class="maincolbox" style='clear:both;' controller="1">
+<div class="maincolcblock ywdblock_coords">
+<div class="wp" style='top:205px;right:169px;position:absolute;' id='ywdautoformat0'><p><a href="http://www.canadahelps.org/CharityProfilePage.aspx?charityID=s80711" target="_blank"><img alt="" src="site/ywd_trialsite_1564/assets/images/screen_shot_2012-08-02_at_5.45.09_pm.png" style="width: 195px; height: 60px;" /></a>&nbsp;</p></div><div style="margin:0px;padding:0px;height:0px;clear:both;"></div>	
+</div>
+</div>
+</div>
+</td>
+<td valign="top" class="rightcolsub">
+<div id="contentrightcol">
+</div>
+</td>
+</tr>
+</table>
+</td>
+</tr>
+</table>
+</td>
+<td id="shadowvert" style="background-repeat:repeat-y;"></td>
+</tr>
+<tr>
+<td id="shadowvertleft">&nbsp;</td>
+<td>
+<div id="contentfooter">
+<div class="maincolbox" style='clear:both;' controller="1">
+<div class="maincolcblock ywdblock_stacked">
+<div class="wp"><h4><span style="color: rgb(255, 255, 255); ">&quot;I was a stranger and you welcomed me...&quot; Matthew 25:35 </span></h4>
+<p><img alt="" height="3" src="site/ywd_trialsite_1564/assets/images/spacer-clear.png" width="13" /></p>
+<h4>&nbsp;</h4>
+<p><span style="color: rgb(0, 204, 255); ">&copy;Matthew House Toronto 2012 &nbsp;| P: 416.364.8287 |&nbsp;981 Dundas Street West Toronto ON M6J 1W4&nbsp;|&nbsp;Twitter&nbsp;|&nbsp;Facebook&nbsp;|&nbsp;Created by&nbsp;GenesisXD</span></p>
+<p><br />
+&nbsp;</p></div><div style="margin:0px;padding:0px;height:0px;clear:both;"></div>	
+</div>
+</div>
+</div>
+</td>
+<td id="shadowvert" style="background-repeat:repeat-y;background-position:bottom;"></td>
+</tr>
+<tr id="shadowbottomrow"><td id="shadowcornerleft"></td><td id="shadowhoriz"></td><td id="shadowcorner" style="background-repeat:no-repeat;"></td></tr>
+<tr>
+<td id="shadowbottomleft"></td>
+<td>
