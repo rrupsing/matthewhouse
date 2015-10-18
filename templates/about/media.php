7bbafd778a42e8d5915c10b386ab0518ae7e11fb
Raul Rupsingh <raul@interaxon.ca>
2015-10-17 23:17:06 -0400

    houses and media

---
+++ templates/about/media.php
@@ -0,0 +1,32 @@
+<?php $this->layout('inner_template', ['title' => 'Matthew House | Refugee Reception Services Toronto', 'page'=>'about', 'subpage' => 'media']) ?>
+
+
+<div id="content">
+<div class="maincolbox" style='clear:both;' controller="1">
+<div class="maincolcblock ywdblock_coords">
+<div class="wp" style='top:205px;left:841px;position:absolute;' id='ywdautoformat0'><p><a href="http://www.canadahelps.org/CharityProfilePage.aspx?charityID=s80711" target="_blank"><img alt="" height="44" src="site/ywd_trialsite_1564/assets/images/screen_shot_2012-08-02_at_5.45.09_pm.png" width="143" /></a>&nbsp;</p></div><div style="margin:0px;padding:0px;height:0px;clear:both;"></div>	
+</div>
+</div>
+<div class="maincolbox" style='clear:both;' controller="1">
+<div class="maincolcblock ywdblock_stacked">
+<div class="wp"><h5><span style="color: rgb(0, 121, 193);">MEDIA</span></h5>
+<p><a href="https://www.thestar.com/news/world/2014/11/14/refugee_journey_especially_tough_for_youth.html">Refugee Journey Especially Tough For Youth</a>&nbsp;is a feature story in the&nbsp;Toronto Star&nbsp;about a former Matthew House teenage resident&nbsp;in a series&nbsp;entitled&nbsp;Migration: Flight of Their Lives, written by award-winning journalist Scott Simmie. &nbsp;<a href="http://www.thestar.com/news/world/2014/11/16/for_frightened_teen_from_zimbabwe_canada_became_a_beacon_of_hope.html">For frightened teen from Zimbabwe, Canada became a beacon of hope</a>&nbsp;is another&nbsp;article by Scott&nbsp;Simmie featuring Dumo, a former&nbsp;unaccompanied teen refugee &nbsp;and now an immigration&nbsp;lawyer&nbsp;who volunteers with Matthew House.</p>
+<p><a href="http://www.nfb.ca/film/everybodys_children">Everybody&rsquo;s Children</a> is a 2008 National Film Board of Canada documentary about two teenage refugees who arrive in Canada as unaccompanied minors, including a 16-year-old boy from Sierra Leone who is helped by Matthew House. ( It can be viewed online by clicking on the title icon above).</p>
+<p><a href="http://www.hgtv.com/divine-design/show/index.html">Video clip from the Divine Design show</a> as Candice Olson and her team transform the living and dining rooms of our Dundas Street West house to make them warm, open and welcoming for residents.</p>
+<p>Author Joey Clifton&rsquo;s 2012 book, The Above Ground Railroad, <a href="http://www.joeyclifton.com/the-above-ground-railroad-the-story-of-the-matthew-house-movement-in-canada">The Story of the Matthew House Movement in Canada</a>, details the founding, growth and expansion of&nbsp; Matthew House from it&#39;s initial inception by our founder Anne Woolger and the subsequent opening of several similar homes across the nation inspired by the Matthew House Toronto model.</p></div><div style="margin:0px;padding:0px;height:0px;clear:both;"></div>	
+</div>
+</div>
+</div>
+</td>
+<td valign="top" class="rightcolsub">
+<div id="contentrightcol">
+</div>
+</td>
+</tr>
+</table>
+</td>
+</tr>
+</table>
+</td>
+<td id="shadowvert" style="background-repeat:repeat-y;"></td>
+</tr>
