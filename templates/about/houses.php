80a170a70eb14b72a1627ef113b719b2df97bdc2
Raul Rupsingh <raul@interaxon.ca>
2015-10-17 23:14:58 -0400

    houses

---
+++ templates/about/houses.php
@@ -0,0 +1,57 @@
+<?php $this->layout('inner_template', ['title' => 'Matthew House | Refugee Reception Services Toronto', 'page'=>'about', 'subpage' => 'houses']) ?>
+
+<div id="content">
+<div class="maincolbox" style='clear:both;' controller="1">
+<div class="maincolcblock ywdblock_coords">
+<div class="wp" style='top:205px;left:841px;position:absolute;' id='ywdautoformat0'><p><a href="http://www.canadahelps.org/CharityProfilePage.aspx?charityID=s80711" target="_blank"><img alt="" height="44" src="site/ywd_trialsite_1564/assets/images/screen_shot_2012-08-02_at_5.45.09_pm.png" width="143" /></a>&nbsp;</p></div><div style="margin:0px;padding:0px;height:0px;clear:both;"></div>	
+</div>
+</div>
+<div class="maincolbox" style='clear:both;' controller="1">
+<div class="maincolcblock ywdblock_stacked">
+<div class="wp"><h5><span style="color: rgb(0, 121, 193);">OUR HOUSES</span></h5>
+<h6><img alt="" height="307" src="site/ywd_trialsite_1564/assets/images/DundasHouse-1.jpg" width="700" /><span style="color: rgb(0, 121, 193); font-size: 18px; line-height: 21px;">DUNDAS STREET WEST</span></h6>
+<p>Our primary shelter on Dundas Street West, near Trinity Bellwoods Park, provides a welcoming home for approximately 80 newly-arrived refugee claimants each year. An average of 12 residents stay at one time, for approximately six to eight weeks.</p>
+<p>Residents typically share a bedroom with a roommate, and everyone contributes by completing assigned daily chores. There is also one larger bedroom that can accommodate a family. Volunteers prepare lunch and dinner each day, and residents eat these meals together around the big dining room table. Weekly orientation sessions and house meetings give residents a chance to get to know each other. A volunteer Night Manager lives in the house to ensure round-the-clock support is available.</p>
+<p>Our staff offices are located at the Dundas Street West house and most of the primary public relations occur at this home. The garage serves as storage for donated furniture and household items, used to equip residents when they are ready to move out into their own homes.</p>
+<h6><img align="bottom" alt="" height="30" src="site/ywd_trialsite_1564/assets/images/blue-dotted-line.png" width="678" /></h6>
+<h6><img alt="" height="307" src="site/ywd_trialsite_1564/assets/images/shirleyhouse.jpg" width="700" /></h6>
+<p><span style="color: rgb(0, 121, 193); font-family: leaguegothicRegular, 'Arial Narrow', droidsansRegular, Arial, Helvetica, sans-serif; font-size: 18px; letter-spacing: 1px; line-height: 21px;">SHIRLEY STREET</span></p>
+<p>Our Shirley Street home provides transitional housing, giving vulnerable residents, like unaccompanied minors, a longer period of time to adjust and find a place to live. Residents live independently here (doing their own shopping and meal preparation) but still in a communal setting. Volunteer house parents live in the house to provide guidance.</p>
+<h6><img align="bottom" alt="" height="30" src="site/ywd_trialsite_1564/assets/images/blue-dotted-line.png" width="678" /></h6>
+<h6><img alt="" height="307" src="site/ywd_trialsite_1564/assets/images/greensides.jpg" width="700" /></h6>
+<h2><span style="color: rgb(0, 121, 193); ">GREENSIDES AVENUE</span></h2>
+<p>Our newest home, on Greensides Avenue, is divided into five separate apartments. Volunteer House Parents live in the main floor apartment, which includes a large, newly-renovated kitchen and gathering space for communal meals. At this house, residents are able to have the independence of living in their own apartment&nbsp;while still enjoying the camaraderie and support of other Matthew House friends.</p></div><div style="margin:0px;padding:0px;height:0px;clear:both;"></div>	
+</div>
+</div>
+</div>
+</td>
+<td valign="top" class="rightcolsub">
+<div id="contentrightcol">
+</div>
+</td>
+</tr>
+</table>
+</td>
+</tr>
+</table>
+</td>
+<td id="shadowvert" style="background-repeat:repeat-y;"></td>
+</tr>
+<tr>
+<td id="shadowvertleft">&nbsp;</td>
+<td>
+<div id="contentfooter">
+<div class="maincolbox" style='clear:both;' controller="1">
+<div class="maincolcblock ywdblock_stacked">
+<div class="wp"><h4><span style="color: rgb(255, 255, 255); ">&quot;I was a stranger and you welcomed me...&quot; Matthew 25:35 </span></h4>
+<p><img src="site/ywd_trialsite_1564/assets/images/spacer-clear.png" width="13" height="3" alt="" /></p>
+<h4></h4>
+<h4><span style="color: rgb(0, 204, 255); ">&copy;Matthew House Toronto 2012 &nbsp;| P: 416.364.8287 |&nbsp;981 Dundas Street &nbsp;Toronto ON M6J 1W4&nbsp;| <a href="http://twitter.com/Matthew_House" target="_blank"><span style="color: rgb(0, 204, 255); ">Twitter</span></a>&nbsp;| </span><a href="https://www.facebook.com/matthewhousetoronto?ref=hl" target="_blank"><span style="color: rgb(0, 204, 255); ">Facebook</span></a><span style="color: rgb(0, 204, 255); ">&nbsp;|&nbsp;</span><span style="color: rgb(0, 204, 255); "><span style="color: rgb(255, 255, 255); ">Created by <a href="http://www.genesisxd.com/" target="_blank"><span style="color: rgb(255, 255, 255); ">GenesisXD</span></a></span><br type="_moz" />
+</span></h4>
+<p></p></div><div style="margin:0px;padding:0px;height:0px;clear:both;"></div>	
+</div>
+</div>
+</div>
+</td>
+<td id="shadowvert" style="background-repeat:repeat-y;background-position:bottom;"></td>
+</tr>
