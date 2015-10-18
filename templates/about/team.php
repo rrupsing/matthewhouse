d1db5502cfb70e4c2ee77ccf43fd7b3654fa021b
Raul Rupsingh <raul@interaxon.ca>
2015-10-17 22:27:24 -0400

    team page

---
+++ templates/about/team.php
@@ -0,0 +1,128 @@
+<?php $this->layout('template', ['title' => 'Matthew House | Refugee Reception Services Toronto']) ?>
+
+<!-- overriding styles -->
+<style type="text/css">
+.contenttable, .contenttablesub {
+	background-image:url(site/ywd_trialsite_1564/assets/images/white-square-background.jpg);
+	background-position:top center;
+	background-repeat:repeat;
+}
+.contentmaincol, .contentmaincolsub, .rightcol, .rightcolsub, .leftcol, .breadcrumbs, #contentfooter {
+	background-color:transparent;
+}
+#contentfooter {
+	background-color:#0079C1;
+}
+body {
+	background-image:url(site/ywd_trialsite_1564/assets/images/ice_age__2X.jpg);
+	background-position:top left;
+	background-repeat:repeat;
+	background-attachment:fixed;
+}
+</style>
+
+<table id="outercontenttable" width="100%" border="0" cellspacing="0" cellpadding="0">
+	<tr>
+		<td>
+			<div id="cse" style="width:100%;display:none;"></div>
+			<table align="left" class="contenttablesub" border="0" cellspacing="0" cellpadding="0">
+				<tr>
+					<td class="leftcol">
+						<div id="navlefttable">
+							<table class="navleft" border="0" cellspacing="0" cellpadding="0">
+								<tr>
+									<td style="padding-left:0px;">
+									</td>
+								</tr>
+								<tr><td class="navleft2offset"></td></tr>
+								<tr><td style="padding-left:0px;"><a href="story.php" class="navleft2">Our Story</a></td></tr><tr><td style="padding-left:0px;"><a href="indexd05b.html?pagepath=About_Us/Our_Team&amp;id=43700" class="navleft2current">Our Team</a></td></tr><tr><td style="padding-left:0px;"><a href="index3993.html?pagepath=About_Us/Our_Houses&amp;id=43698" class="navleft2">Our Houses</a></td></tr><tr><td style="padding-left:0px;"><a href="indexee2f.html?pagepath=About_Us/Media&amp;id=44842" class="navleft2">Media</a></td></tr><tr><td style="padding-left:0px;"><a href="index5a2e.html?pagepath=About_Us/News&amp;id=43703" class="navleft2">News</a></td></tr><tr><td style="padding-left:0px;"><a href="index2c1c.html?pagepath=About_Us/Newsletter&amp;id=68762" class="navleft2">Newsletter</a></td></tr>
+							</table>
+						</div>
+					</td>
+					<td class="contentmaincolsub">
+						<div id="content">
+							<div class="maincolbox" style='clear:both;width:750px;' controller="1">
+								<div class="maincolcblock ywdblock_stacked">
+									<div class="wp" style='min-height:800px;' id='ywdautoformat0'><h5><font color="#0079c1">MATTHEW HOUSE TEAM&nbsp;</font><img align="bottom" alt="" height="10" src="site/ywd_trialsite_1564/assets/images/spacer-clear.png" style="line-height: 15px; letter-spacing: 1px; font-family: droidsansRegular, Arial, Helvetica, sans-serif; font-size: 12px;" width="45" /></h5>
+										<p><strong>Executive Director, Interim:</strong>&nbsp;<span style="color: rgb(51, 102, 255);">Karen Francis</span></p>
+										<p>P:&nbsp;<a href="tel:647-344-0515">647-344-0515</a>&nbsp;&nbsp;E: <a href="index.html" target="_blank">karenfrancis@matthewhouse.ca</a></p>
+										<p>With a background in communications and a heart for people, Karen ensures that the mission and vision of Matthew House is executed with a high level of excellence.</p>
+										<p>______________________________________________________________________________________________________________________________________________________________________________________</p>
+										<p><strong>Founder,</strong>&nbsp; <span style="color: rgb(51, 102, 204);">Anne Woolger</span></p>
+										<p>P:<span style="color: rgb(51, 102, 204);">&nbsp;416</span>-<font color="#3366cc">364-8287</font> E:<span style="color: rgb(51, 153, 255);"> <a href="mailto:anne@matthewhouse.ca">anne@matthewhouse.ca</a></span></p>
+										<p>With a passion for justice&nbsp; and desire to create welcoming communities, Anne&nbsp;has&nbsp;been&nbsp;helping resettle refugees for more than 25 years.&nbsp; Her&nbsp;vision was the catalyst behind the establishment of Matthew House in 1998.&nbsp; ( See her full bio below).</p>
+										<hr />
+										<p><strong>Operations Director:</strong>&nbsp;<span style="color: rgb(51, 102, 255);">Astar </span><span style="color: rgb(51, 102, 255);"><span>Anjajo</span></span></p>
+										<p>P:&nbsp;647.344.0515&nbsp;&nbsp;E: <a href="mailto:astar@matthewhouse.ca">astar@matthewhouse.ca</a></p>
+										<p>Astar oversees our three homes; ensuring that they are operating efficiently and that our guests are supported.</p>
+										<hr />
+										<p><strong>Settlement Coordinator:</strong><span style="color: rgb(51, 102, 255);">&nbsp;Said Nixon Bangura&nbsp;&nbsp;</span><br />
+											P: <a href="tel:416.203.7848">416.203.7848</a> &nbsp;E:&nbsp;<a href="mailto:said@matthewhouse.ca">said@matthewhouse.ca</a></p>
+											<p>A warm smile from Said greets you when you arrive at Matthew House. A former refugee himself, Said knows the refugee settlement process well and ensures that our guests get the best possible support.</p>
+											<hr />
+											<p><strong>Refugee and Advocacy Coordinator: </strong><span style="color: rgb(51, 102, 255);">Samia Tecle</span></p>
+											<p>P: <a href="tel:416.203.7848">416.203.7848</a> &nbsp;E:&nbsp;<a href="index.html"><span style="color: rgb(0, 0, 205);">refugeeclaim@matthewhouse.ca</span></a></p>
+											<p>Samia&#39;s assists our guests as they work together preparing for their refugee hearing.</p>
+											<hr />
+											<p><strong>Development and Communications Coordinator</strong>: <span style="color: rgb(51, 102, 255);">Krystle Henry&nbsp;</span></p>
+											<p>P: &nbsp;<a href="tel:416.364.8287">416.364.8287</a> &nbsp;E:&nbsp;<a href="mailto:krystle@matthewhouse.ca">krystle@matthewhouse.ca</a></p>
+											<p>Krystle comes from a communications background and works to keep everyone in the Matthew House family informed on the latest happenings in our three homes.&nbsp;</p>
+											<hr />
+											<p><strong>Home Coordinator:&nbsp;</strong><span style="color: rgb(51, 102, 255);">Karyn Atkins</span><br />
+												P:&nbsp;<a href="tel:416.203.7848">416.203.7848</a></p>
+												<p>Karyn can often be found in our kitchen ensuring that delicious meals are being prepared for our guests. She always has a friendly word for everyone and helps to keep all the domestic aspects of our home running smoothly.</p>
+												<hr />
+												<p><strong>Move Coordinator: </strong><font color="#3366ff">Brian Thrower</font><strong><font color="#3366ff">&nbsp;</font></strong><br />
+													P:&nbsp;<a href="tel:905-877-3647">905-877-3647</a> &nbsp;E:&nbsp;<a href="mailto:brianethrower@yahoo.ca">brianethrower@yahoo.ca</a></p>
+													<p>Brian sources donations for our guests and helps them move into their own apartments.</p>
+													<hr />
+													<p><strong>Bookkeeper:&nbsp;</strong><span style="color: rgb(51, 102, 255);">Carol Bain</span></p>
+													<p>Carol brings a calm efficient presence to our sometimes chaotic and lively home on Thursdays. Her years of experience and expertise bookkeeping for non-profit charities are great assets to our ministry.</p>
+													<hr />
+													<p><strong>Dundas St. Night Manager: </strong><span style="color: rgb(51, 102, 255);">&nbsp;David Fink</span></p>
+													<p>David welcomes residents and ensures our home is safe and secure overnight.</p>
+													<p><img alt="" height="30" src="site/ywd_trialsite_1564/assets/images/blue-dotted-line.png" width="678" /></p>
+													<h6>HOUSE PARENT TEAMS</h6>
+													<p>&nbsp;</p>
+													<p><strong>Shirley Street House:</strong> <font color="#3366ff">Carrie and Chris Turman</font></p>
+													<p>English teaching and &quot;people connecting&quot;&nbsp;are a few of the many gifts that Chris and Carrie bring&nbsp;to our home on Shirley St. Their excellent relational skills make them very popular among the diverse residents who live in this home.</p>
+													<hr />
+													<p><strong>Greensides Avenue House:&nbsp; </strong><span style="color: rgb(51, 102, 255);">Tim and Julie McCoy</span></p>
+													<p>Tim and Julie both have&nbsp;many years of experience in community building and enjoy offering hospitality&nbsp;at our Greensides Ave. home.</p>
+													<p><img alt="" height="30" src="site/ywd_trialsite_1564/assets/images/blue-dotted-line.png" style="line-height: 14px; letter-spacing: 1px; font-family: leaguegothicRegular, &quot;Arial Narrow&quot;, droidsansRegular, Arial, Helvetica, sans-serif; font-size: 10px;" width="678" /></p>
+													<h6>BOARD MEMBERS</h6>
+													<h2>&nbsp;</h2>
+													<h2>Dr. Clement A. Jumbe</h2>
+													<p>Since coming to Canada as a refugee from Zimbabwe in 2005, Clement has completed his PhD at the University of Toronto through the Scholars at Risk Program and now teaches in the Institute for Global Citizenship and Equity at Centennial College.&nbsp;&quot;<em>As a former resident, I learned that there was an opportunity for kindness at Matthew House and my wish as a Board Member is that giving hope to refugees should not stop but continue forever</em>.&rdquo;</p>
+													<h2>Carolyn Madonia</h2>
+													<p>Carolyn has been a regular volunteer at Matthew House for several years, including as the editor of the newsletter Inside Matthew House. &ldquo;<em>In the time that I have been involved with Matthew House I have had the privilege of meeting amazing and inspiring people among the staff, residents and volunteers there</em>.&rdquo;</p>
+													<h2>Greg Smith</h2>
+													<p>Greg, a human resources leader with over 25 years experience, currently heads the HR function for Wolseley Canada.<br />
+														&quot;<em>I first connected with MH when I dropped off a food donation for a friend and immediately felt the love, compassion and impact of this wonderful community.&quot;</em></p>
+														<h2>Steve Tso</h2>
+														<p>Stephen is a marketing and operations professional and has been volunteering at Matthew House for several years. &quot;<em>I believe God is working through Matthew House to reach people who normally may not have the opportunity to experience Him. It&#39;s an honour to be a part of this great ministry here in the heart of Toronto.</em>&rdquo;</p>
+														<h2>John Winsor</h2>
+														<p>John is a Human Resources Executive with over 20 years&rsquo; experience, mainly with TD Bank Group. &nbsp;He &nbsp;currently heads&nbsp;the retail H.R. function for Oxford.&nbsp;&ldquo;<em>It&rsquo;s profoundly humbling to have the honour to be involved with individuals who have experienced so much adversity yet continue to find ways to overcome and achieve</em>.&rdquo;</p>
+														<h2>Paul Zurbrigg</h2>
+														<p>Paul has taught high school science at Bramalea Secondary School for the past 25 years and is a long-time volunteer at Matthew House. &quot;<em>I enjoy connecting on a personal level with residents and have met many wonderful people through Matthew House.</em>&quot;</p>
+														<h2><img alt="" src="site/ywd_trialsite_1564/assets/images/blue-dotted-line.png" style="width: 678px; height: 30px;" /></h2>
+														<h2>Founder: Anne Woolger</h2>
+														<p>P: &nbsp;416.364.8287&nbsp;&nbsp;E:&nbsp;<a href="mailto:anne@matthewhouse.ca">anne@matthewhouse.ca</a></p>
+														<p>As the founding director of Matthew House, Anne has been working with refugees in Toronto since 1988. She&nbsp;has a BA (Hons) from&nbsp;Queen&rsquo;s University and a&nbsp;Master&#39;s degree in Theology from&nbsp;Tyndale University College and Seminary. She also holds a certificate in International Human Rights Law from the International Institute of Human Rights, Strasbourg, France. Since beginning her work at a city-run shelter for refugees in 1988, Anne has helped with the hands-on resettlement of approximately 4,000 refugees in Toronto. Anne&rsquo;s vision was the catalyst behind the establishment of Matthew House Toronto as well seven other refugee shelters across the country. Anne has served on both local and international refugee committees. She has represented the Canadian Council of Churches at United Nations High Commissioner for Refugees (UNHCR) meetings in Geneva and at other human rights consultations. Her desire is to encourage and inspire Canadians to overcome their fears and reach out and welcome the refugees arriving on our doorstep. In 2002 Anne received the &ldquo;Distinguished Alumni&rdquo; award from her alma mater (Tyndale ) and in 2013 she was awarded a Queen Elizabeth II Diamond Jubilee Medal in recognition for of her work&nbsp;with refugees in Canada. She has traveled extensively including to the Kurdish enclave of Northern Iraq. She loves travelling, kayaking and swimming and has a growing passion for gardening, having recently completed requirements for a Certificate in Garden Design through George Brown College.</p></div><div style="margin:0px;padding:0px;height:0px;clear:both;"></div>	
+													</div>
+												</div>
+												<div class="maincolbox" style='clear:both;' controller="1">
+													<div class="maincolcblock ywdblock_coords">
+														<div class="wp" style='top:205px;right:209px;position:absolute;' id='ywdautoformat0'><p><a href="http://www.canadahelps.org/CharityProfilePage.aspx?charityID=s80711" target="_blank"><img alt="" height="44" src="site/ywd_trialsite_1564/assets/images/screen_shot_2012-08-02_at_5.45.09_pm.png" width="143" /></a>&nbsp;</p></div><div style="margin:0px;padding:0px;height:0px;clear:both;"></div>	
+													</div>
+												</div>
+											</div>
+										</td>
+									</tr>
+								</table>
+							</td>
+						</tr>
+					</table>
+				</td>
+				<td id="shadowvert" style="background-repeat:repeat-y;"></td>
+			</tr>
