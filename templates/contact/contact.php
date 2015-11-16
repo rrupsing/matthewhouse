9f72295e6f324ba7e92d2c8b9cd8ad66d0784eec
Raul Rupsingh <raul@interaxon.ca>
2015-11-15 22:04:53 -0500

    adding events & contact info

---
+++ templates/contact/contact.php
@@ -0,0 +1,69 @@
+<?php $this->layout('inner_template', ['title' => 'Matthew House | Refugee Reception Services Toronto', 'page'=>'contact', 'subpage' => 'contact']) ?>
+
+
+<div id="content">
+<div class="maincolbox" style='clear:both;' controller="1">
+<div class="maincolcblock ywdblock_stacked">
+<div class="wp"><h5>CONTACT US!</h5>
+<p><strong><span style="color: rgb(0, 0, 0);">By phone:</span></strong></p>
+<p><strong><font color="#0000ff">Administration Office</font><span style="color: rgb(0, 121, 193);">:</span></strong> 416.364.8287</p>
+<p><span style="color: rgb(0, 0, 255);"><strong>Operations Director</strong></span>: 647-344-0515</p>
+<p><strong><span style="color: rgb(0, 0, 255);">Main Shelter</span><span style="color: rgb(0, 121, 193);">:</span></strong> 416.203.7848</p>
+<p>&nbsp;</p>
+<p><strong>By Fax: &nbsp;</strong>416-203-7848</p>
+<p>&nbsp;</p>
+<p><strong>By email:</strong></p>
+<p><strong><span style="color: rgb(0, 0, 255);">Email general inquiries</span><span style="color: rgb(0, 121, 193);">:</span></strong> <a href="mailto:admin@matthewhouse.ca">admin@matthewhouse.ca</a></p>
+<p><span style="color: rgb(0, 0, 255);"><strong>Interim Executive Director</strong></span>:&nbsp;<a href="mailto:Karen@matthewhouse.ca">karenfrancis@matthewhouse.ca</a></p>
+<p><span style="color: rgb(0, 0, 255);"><strong>Founding Director</strong>:</span> <a href="mailto:anne@matthewhouse.ca">anne@matthewhouse.ca</a></p>
+<p><strong><span style="color: rgb(0, 0, 255);">Operations Director</span></strong>: <a href="index.html">astar@matthewhouse.ca</a></p>
+<p>Find our complete staff list on the <a href="indexd05b.html?pagepath=About_Us/Our_Team&amp;id=43700">Our Team </a>page</p>
+<p>&nbsp;</p>
+<p><strong>C</strong><strong>o</strong><strong>rrespondence can be directed to:</strong></p>
+<p>Matthew House<br />
+981 Dundas Street West<br />
+Toronto, ON, Canada&nbsp; M6J 1W4</p>
+<p>&nbsp;</p>
+<h4><span style="color: rgb(128, 128, 128);">*Please note that Matthew House does NOT engage in overseas refugee sponsorship and is not able to<br />
+reply to inquiries from overseas related to refugee sponsorship.</span></h4>
+<h4>&nbsp;</h4>
+<p>&nbsp;</p>
+<p>&nbsp;</p></div><div style="margin:0px;padding:0px;height:0px;clear:both;"></div>	
+</div>
+</div>
+<div class="maincolbox" style='clear:both;' controller="1">
+<div class="maincolcblock ywdblock_stacked">
+<div class="nwp">
+<!-- Google Maps Element Code -->
+<iframe width="425" height="350" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="http://maps.google.com/maps?maptype=roadmap&amp;f=q&amp;iwloc=&amp;source=s_q&amp;hl=en&amp;geocode=true&amp;q=981%20Dundas%20St.%20W%20Toronto,%20ON,%20Canada&amp;aq=0&amp;ie=UTF8&amp;hq=&amp;hnear=981%20Dundas%20St.%20W%20Toronto,%20ON,%20Canada&amp;z=14&amp;output=embed&amp;element=true"></iframe><br /> 
+<div style="margin:0px;padding:0px;height:0px;clear:both;"></div>
+<!-- Google Search Result Snippet Ends -->
+</div>	
+</div>
+</div>
+<div class="maincolbox" style='clear:both;' controller="1">
+<div class="maincolcblock ywdblock_coords">
+<div class="wp" style='top:205px;left:841px;position:absolute;' id='ywdautoformat0'><p><a href="http://www.canadahelps.org/CharityProfilePage.aspx?charityID=s80711" target="_blank"><img alt="" height="44" src="site/ywd_trialsite_1564/assets/images/screen_shot_2012-08-02_at_5.45.09_pm.png" width="143" /></a>&nbsp;</p></div><div style="margin:0px;padding:0px;height:0px;clear:both;"></div>	
+</div>
+</div>
+<div class="maincolbox" style='clear:both;' controller="1">
+<div class="maincolcblock ywdblock_stacked">
+<div class="wp"><p>&nbsp;<img src="site/ywd_trialsite_1564/assets/images/blue-dotted-line-1.png" width="678" height="30" alt="" /></p>
+<p><img src="site/ywd_trialsite_1564/assets/images/connect-w-us-1.png" width="110" height="58" alt="" />&nbsp; &nbsp;<a href="https://www.facebook.com/matthewhousetoronto" target="_blank"><img src="site/ywd_trialsite_1564/assets/images/fb-icon-1.png" width="43" height="58" alt="" /></a>&nbsp;<a href="https://twitter.com/Matthew_House"><img src="site/ywd_trialsite_1564/assets/images/twitter-icon-1.png" width="38" height="58" alt="" /></a></p>
+<p><img src="site/ywd_trialsite_1564/assets/images/blue-dotted-line-2.png" width="678" height="30" alt="" /></p></div><div style="margin:0px;padding:0px;height:0px;clear:both;"></div>	
+</div>
+</div>
+</div>
+</td>
+<td valign="top" class="rightcolsub">
+<div id="contentrightcol">
+</div>
+</td>
+</tr>
+</table>
+</td>
+</tr>
+</table>
+</td>
+<td id="shadowvert" style="background-repeat:repeat-y;"></td>
+</tr>
---
+++ templates/events/events.php
@@ -0,0 +1,50 @@
+<?php $this->layout('inner_template', ['title' => 'Matthew House | Refugee Reception Services Toronto', 'page'=>'events', 'subpage' => 'events']) ?>
+
+<div id="content">
+<div class="maincolbox" style='clear:both;' controller="1">
+<div class="maincolcblock ywdblock_coords">
+<div class="wp" style='top:120px;left:29px;position:absolute;' id='ywdautoformat0'><h3><strong>EVENTS</strong></h3>
+<p>&nbsp;</p></div><div style="margin:0px;padding:0px;height:0px;clear:both;"></div>	
+</div>
+</div>
+<div class="maincolbox" style='clear:both;' controller="1">
+<div class="maincolcblock ywdblock_stacked">
+<div class="nwp">
+<div class="eventslistdiv">
+<table class="eventslisttable" border="0" cellspacing="0" cellpadding="0">
+<tr>
+<td class="eventlistics"> 
+</td> 
+<td class="eventslistdate" valign="top">
+Sunday, October 18, 2015
+<br />8:00 AM
+</td>
+<tr>
+<td></td>
+<td class="eventslisttitle" valign="top"><h3>
+<a href="index969d.html?eventid=8256&amp;pagepath=Events&amp;id=49964#event8256">
+Scotiabank Charity Run</a>
+</h3></td>
+</tr>
+</tr>
+</table>
+</div>
+<div style="margin:0px;padding:0px;height:0px;clear:both;"></div>
+<!-- Google Search Result Snippet Ends -->
+</div>	
+</div>
+</div>
+</div>
+</td>
+<td valign="top" class="rightcolsub">
+<div id="contentrightcol">
+</div>
+</td>
+</tr>
+</table>
+</td>
+</tr>
+</table>
+</td>
+<td id="shadowvert" style="background-repeat:repeat-y;"></td>
+</tr>
