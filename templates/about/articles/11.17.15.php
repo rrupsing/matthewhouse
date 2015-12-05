3fdb1d2840518683cd72353c7fbd17ffe694cbe8
Raul Rupsingh <raul@interaxon.ca>
2015-12-05 17:16:54 -0500

    adding two new events

---
+++ templates/about/articles/11.17.15.php
@@ -0,0 +1,35 @@
+<div class="newstitle">
+<h2>Matthew House affirms Statement of Canadian Council for Refugees regarding Recent Events</h2>
+</div>
+<p></p>
+<p>11.17.15</p>
+<div class="newsbody"><p><span style="color: rgb(0, 0, 205);">&nbsp;As a member agency of the Canadian Council for Refugees, Matthew House affirms the following statement:</span></p>
+<p>Statement in response to attacks in Paris and Beirut<br />
+<br />
+The CCR&nbsp;&nbsp;joins in the global outrage at the recent mass murders in Paris and Beirut. The loss of life and shattering of our sense of security connects us to the daily death and destruction in Syria and in other countries at war. We hope that Canadians will remember that Syrian refugees are victims of this violence and will redouble their commitment to welcome them in Canada.<br />
+<br />
+We are dismayed by some public statements unfairly associating refugees with security risks. We would like to point out the &nbsp; &nbsp;following:<br />
+<br />
+- Refugees are people who are fleeing violence and are looking for security. They are not a threat to security.<br />
+<br />
+- Refugees who come to Canada are subjected to rigorous security screening, whether they are selected abroad for resettlement or they make a refugee claim in Canada. The Canadian government has a well-developed and sophisticated system for doing security checks, through Canada Border Services Agency and CSIS. They review personal information of the applicants and assess them according to certain factors that might point to a possible risk. If applicants fit a profile of concern, they are subjected to more detailed screening, including if necessary an interview with a CSIS agent. Processing of &nbsp; &nbsp;such applicants is delayed while the security check is conducted.<br />
+<br />
+- Everyone considered for resettlement must be referred by either the UNHCR or by private sponsors in Canada. It is not possible for someone to propose themselves.<br />
+&nbsp;&nbsp;&nbsp; - The UNHCR refers for resettlement only a tiny portion of registered refugees. They refer refugees who are particularly vulnerable (e.g. a widow with several young children, LGBT refugees who at risk in their place of temporary refuge, or someone who has health issues).<br />
+<br />
+&nbsp;&nbsp;&nbsp; - Private sponsors name people that they know, often family members of&nbsp; people in Canada.<br />
+<br />
+- The events of last week do not require any change in Canada&rsquo;s approach to Syrian refugees, since ensuring a thorough security check was always a mandatory part of the plan. The Minister of Immigration, Refugees and Citizenship has consistently said that processing will be done as fast as possible, but without compromising necessary security checks.<br />
+<br />
+- Every day thousands of people enter Canada other than as refugees. It is unfair that refugees are singled out for suspicion. People planning to come to Canada to commit crimes would be unlikely to try to come as refugees since processing is often difficult and they would be subjected to security screenings. It appears that most of those committing the attacks in Paris were French or Belgian citizens: European citizens can easily enter Canada without any security screening.<br />
+<br />
+- The individuals who have committed recent terrorist attacks in Canada have been born in this country.<br />
+<br />
+Every day Syrian refugee children in the Middle East go hungry. Winter is coming and their situation will grow worse. Syrian refugees have more to be fearful of than Canadians do.<br />
+<br />
+Opening our doors to refugees is one of the strongest ways possible to reject the violence and the incitement to hatred and division represented by the attacks.&nbsp;</p>
+<p>&nbsp;</p>
+<p>CCR, Nov. 17, 2015.<br />
+<br />
+&nbsp;</p></div>
+<div class="contentbottomlinksgoback">
---
+++ templates/about/articles/11.29.15.php
@@ -0,0 +1,13 @@
+<div class="newstitle">
+<h2>Matthew House Founder Anne Woolger featured in CBC article on Refugee Children</h2>
+</div>
+<p></p>
+<p>11.29.15</p>
+<div class="newsbody"><p>&nbsp;</p>
+<p>Please click on link below for CBC article on the needs of refugee children:</p>
+<p><a href="http://www.cbc.ca/news/canada/child-refugees-assistance-welcome-canada-1.3339971?__vfz=tc%3DPMEFQp31zs">http://www.cbc.ca/news/canada/child-refugees-assistance-welcome-canada-1.3339971?__vfz=tc%3DPMEFQp31zs</a></p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p></div>
+<div class="contentbottomlinksgoback">
