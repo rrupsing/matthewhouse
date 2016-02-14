91a2759d06cb505623410aecdcc504c899d64883
Raul Rupsingh <raul@interaxon.ca>
2016-02-14 16:29:49 -0500

    linking pdf on our site via ywd

--- templates/main.php
+++ templates/main.php
@@ -171,7 +171,7 @@ $recent_posts = wp_get_recent_posts(array(
 					<div class="maincolcblock ywdblock_coords">
 						<div class="wp" style='top:800px;left:459px;position:absolute;' id='ywdautoformat0'>
 							<p>
-							<a href="http://ow.ly/Vl8gx">
+							<a href="http://www.matthewhouse.ca/matthewhouse/resources/files/2016wintercatalogue_giftofhope.pdf">
 								<img alt="" src="site/ywd_trialsite_1564/assets/images/giftofhope_banner-1.jpg" style="width: 513px; height: 190px;" />
 							</a>
 							</p>
