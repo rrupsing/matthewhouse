d12a49c83f7d6016131c8dc0a8a113e9ce3fd946
Raul Rupsingh <raul@interaxon.ca>
2016-02-28 15:00:01 -0500

    adding volunteer site via wordpress page instead

--- templates/volunteer/volunteer.php
+++ templates/volunteer/volunteer.php
@@ -3,34 +3,12 @@
 <div id="content">
 <div class="maincolbox" style='clear:both;' controller="1">
 <div class="maincolcblock ywdblock_stacked">
-<div class="wp"><h5><span style="color:0099FF;">&nbsp;VOLUNTEERS, WE NEED YOU!</span></h5></div><div style="margin:0px;padding:0px;height:0px;clear:both;"></div>	
-</div>
-</div>
-<div class="maincolbox" style='clear:both;' controller="1">
-<div class="maincolcblock ywdblock_stacked">
-<div class="wp"><p>There are many ways you can get involved with the work of Matthew House, from preparing a meal once-a-week to helping at Matthew House Camp. Our volunteers shop for groceries, sort donations, escort residents to appointments, maintain our houses, and act as translators. Occasionally we have projects, like home renovations or preparing a mailing, that require extra teams of helpers. Once a month, groups prepare a large meal for our Matthew House Club. <span style="color: rgb(0, 121, 193);">Often the most important role of our volunteers is to simply befriend a refugee and provide an invaluable personal connection.</span></p>
-<p>To find out more about volunteer opportunities at Matthew House, we host regular Volunteer Orientations on the second Monday of the month. This is your opportunity to come and tour our shelter, hear details of our work and learn about volunteer opportunities first hand. See below to find the upcoming dates of our next orientation sessions&nbsp;or <a href="mailto:volunteer@matthewhouse.ca">contact our Volunteer Coordinator</a>.</p></div><div style="margin:0px;padding:0px;height:0px;clear:both;"></div>	
-</div>
-</div>
-<div class="maincolbox" style='clear:both;' controller="1">
-<div class="maincolcblock ywdblock_stacked">
-<div class="wp"><p><span style="color: rgb(0, 121, 193);">THE NEXT VOLUNTEER ORIENTATION DATES ARE:</span></p>
-<p>Feb 29, 2016</p>
-<p>April 11, 2016</p>
-<p>May 22, 2016</p>
-<p>June 27, 2016</p>
-<p><strong>Sessions begin at 7:00 pm at our home at 981 Dundas St. West, Toronto</strong></p>
-<p>To RSVP please COMPLETE the Volunteer form, noting the orientation&nbsp;date you would like to attend&nbsp;and email our&nbsp;<span style="color: rgb(0, 121, 193);"><a href="mailto:volunteer@matthewhouse.ca?subject=Volunteer%20Opportunity">volunteer</a>&nbsp;</span><a href="mailto:volunteer@matthewhouse.ca?subject=Volunteer%20Opportunity">coordinator</a></p>
-<p>&nbsp;<img alt="" height="30" src="site/ywd_trialsite_1564/assets/images/blue-dotted-line.png" width="678" /></p>
-<h6><span style="color:0099FF;">LOOKING TO VOLUNTEER?</span><a href="page.php?p=volunteer&s=contact" target="_self"><span style="color:0099FF;"><img align="right" alt="" height="61" src="site/ywd_trialsite_1564/assets/images/fill-out-form.png" width="214" /></span></a></h6>
-<p><img alt="" height="10" src="site/ywd_trialsite_1564/assets/images/spacer-clear.png" width="45" /></p>
-<p>Please complete our volunteer form and sign up for a Volunteer Orientation session as this a required field in the volunteer form. After attending the orientation session, we will contact you to discuss the next steps of our intake process (which&nbsp;may&nbsp;include references and&nbsp;security checks) in order for a mutually satisfying volunteer schedule to be arranged.&nbsp; Thank you!</p></div><div style="margin:0px;padding:0px;height:0px;clear:both;"></div>	
-</div>
-</div>
-<div class="maincolbox" style='clear:both;' controller="1">
-<div class="maincolcblock ywdblock_stacked">
-<div class="wp"><p>&nbsp;<img alt="" height="30" src="site/ywd_trialsite_1564/assets/images/blue-dotted-line.png" width="678" /><img alt="" height="118" src="site/ywd_trialsite_1564/assets/images/quote-congo-1.png" width="700" /></p></div><div style="margin:0px;padding:0px;height:0px;clear:both;"></div>	
-</div>
+
+<iframe src="http://www.matthewhouse.ca/wordpress/volunteers-we-need-you/" style="width: 90%; height: 600px"
+scrolling="no" marginwidth="0" marginheight="0" frameborder="0" vspace="0" hspace="0">
+</iframe>
+
+
 </div>
 <div class="maincolbox" style='clear:both;' controller="1">
 <div class="maincolcblock ywdblock_coords">
