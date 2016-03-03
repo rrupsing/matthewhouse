10833bd92517851287b73b52976d2570383d7758
Raul Rupsingh <raul@interaxon.ca>
2016-03-03 00:29:35 -0500

    updating team page to also be through wordpress

--- templates/about/team.php
+++ templates/about/team.php
@@ -3,15 +3,23 @@
 						<div id="content">
 							<div class="maincolbox" style='clear:both;width:750px;' controller="1">
 								<div class="maincolcblock ywdblock_stacked">
-
+<!--
 <iframe src="http://www.matthewhouse.ca/wordpress/matthew-house-team/" style="width: 90%; height: 3000px"
 scrolling="no" marginwidth="0" marginheight="0" frameborder="0" vspace="0" hspace="0">
 </iframe>
+-->
+<?php
+	
+	include('../wordpress/wp-load.php'); 
+	$page = get_page_by_title('Matthew House Team');
+	echo $page->post_content;
+?>
+
 
 
 												<div class="maincolbox" style='clear:both;' controller="1">
 													<div class="maincolcblock ywdblock_coords">
-														<div class="wp" style='top:205px;right:209px;position:absolute;' id='ywdautoformat0'><p><a href="http://www.canadahelps.org/CharityProfilePage.aspx?charityID=s80711" target="_blank"><img alt="" height="44" src="site/ywd_trialsite_1564/assets/images/screen_shot_2012-08-02_at_5.45.09_pm.png" width="143" /></a>&nbsp;</p></div><div style="margin:0px;padding:0px;height:0px;clear:both;"></div>	
+														<div class="wp" style='top:150px;right:209px;position:absolute;' id='ywdautoformat0'><p><a href="http://www.canadahelps.org/CharityProfilePage.aspx?charityID=s80711" target="_blank"><img alt="" height="44" src="site/ywd_trialsite_1564/assets/images/screen_shot_2012-08-02_at_5.45.09_pm.png" width="143" /></a>&nbsp;</p></div><div style="margin:0px;padding:0px;height:0px;clear:both;"></div>	
 													</div>
 												</div>
 											</div>
