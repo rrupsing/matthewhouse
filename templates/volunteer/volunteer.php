f5040925dd8eeebaedd9c2339f6a5906367a4710
Raul Rupsingh <raul@interaxon.ca>
2016-03-20 17:38:05 -0400

    fixing donate button on volunteer page

--- templates/volunteer/volunteer.php
+++ templates/volunteer/volunteer.php
@@ -19,7 +19,7 @@ scrolling="no" marginwidth="0" marginheight="0" frameborder="0" vspace="0" hspac
 </div>
 <div class="maincolbox" style='clear:both;' controller="1">
 <div class="maincolcblock ywdblock_coords">
-<div class="wp" style='top:205px;left:841px;position:absolute;' id='ywdautoformat0'><p><a target="_blank" href="http://www.canadahelps.org/CharityProfilePage.aspx?charityID=s80711"><img width="143" height="44" alt="" src="site/ywd_trialsite_1564/assets/images/screen_shot_2012-08-02_at_5.45.09_pm.png" /></a>&nbsp;</p></div><div style="margin:0px;padding:0px;height:0px;clear:both;"></div>	
+<div class="wp" style='top:135px;left:841px;position:absolute;' id='ywdautoformat0'><p><a target="_blank" href="http://www.canadahelps.org/CharityProfilePage.aspx?charityID=s80711"><img width="143" height="44" alt="" src="site/ywd_trialsite_1564/assets/images/screen_shot_2012-08-02_at_5.45.09_pm.png" /></a>&nbsp;</p></div><div style="margin:0px;padding:0px;height:0px;clear:both;"></div>	
 </div>
 </div>
 </div>
