42935b6d279f3cbd398713a93162ac6ff52aff14
Raul Rupsingh <rupsingh@gmail.com>
2016-11-26 14:40:18 -0500

    shrinking new image

--- templates/main.php
+++ templates/main.php
@@ -217,11 +217,11 @@ $recent_posts = wp_get_recent_posts(array(
 								<img alt="" src="site/ywd_trialsite_1564/assets/images/giftofhope_banner-1.jpg" style="width: 513px; height: 190px;" />
 							</a>
 						-->
-
+							<!--
 							<script id="ch_cdn_embed" type="text/javascript" data-page-id="29049" data-cfasync="false" src="https://www.canadahelps.org/services/wa/js/apps/donatenow/embed.min.js"></script>
-
+							-->
 							<a target="_blank" href="https://www.canadahelps.org/dn/29049">
-								<img alt="" src="site/ywd_trialsite_1564/assets/images/yearend_banner.png" style="height: 290px;" />
+								<img alt="" src="site/ywd_trialsite_1564/assets/images/yearend_banner.png" style="height: 195px;" />
 							</a>
 
 							</p>
